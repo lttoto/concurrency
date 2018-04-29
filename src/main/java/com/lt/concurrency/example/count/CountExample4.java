74b6f1c7a444122bbbb3b6a546740c4ff66fdcae
liutao <18616948061@163.com>
2018-04-29 15:01:28 +0800

    AQS中volatile的各种实例

---
+++ src/main/java/com/lt/concurrency/example/count/CountExample4.java
@@ -0,0 +1,50 @@
+package com.lt.concurrency.example.count;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by taoshiliu on 2018/4/29.
+ *  volatile内存可见性
+ *  volatile不能解决线程安全
+ *  volatile适合修饰标识位
+ */
+@Slf4j
+public class CountExample4 {
+
+    public static int clinetTotal = 5000;
+
+    public static int threadTotal = 200;
+
+    public static volatile int count = 0;
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clinetTotal);
+        for(int i = 0;i < clinetTotal;i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e) {
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",count);
+    }
+
+    private static void add() {
+        count++;
+    }
+
+}
