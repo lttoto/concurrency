284df5b0f07d5859ee46e893b4e1a0397b4911a7
liutao <18616948061@163.com>
2018-04-30 14:56:20 +0800

    CyclicBarrier同步组件 核心方法 CyclicBarrier cyclicBarrier = new CyclicBarrier(num) cyclicBarrier.await() CyclicBarrier cyclicBarrier = new CyclicBarrier(num,() -> {      doSomething(); })

---
+++ src/main/java/com/lt/concurrency/example/aqs/CyclicBarrierExample2.java
@@ -0,0 +1,42 @@
+package com.lt.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by taoshiliu on 2018/4/30.
+ */
+@Slf4j
+public class CyclicBarrierExample2 {
+
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(5,() -> {
+        log.info("callback is running");
+    });
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for(int i = 0;i < 10;i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executorService.execute(() -> {
+                try {
+                    race(threadNum);
+                }catch (Exception e) {
+                    log.error("exception",e);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception{
+        Thread.sleep(1000);
+        log.info("{} is ready",threadNum);
+        cyclicBarrier.await();
+        log.info("{} continue",threadNum);
+    }
+}
